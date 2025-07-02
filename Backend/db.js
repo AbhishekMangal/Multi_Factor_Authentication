d16dcbc0f2dbd893289e15a6cc58b6f3c3723eaf
AbhishekMangal <149200926+AbhishekMangal@users.noreply.github.com>
2025-07-02 13:18:31 +0530

    fix issue

--- Backend/db.js
+++ Backend/db.js
@@ -4,7 +4,10 @@ require('dotenv').config();
 const connectToMongo = async(username, password)=> {
     try {
       
-        await mongoose.connect(`mongodb+srv://${username}:${[password]}@cluster0.sxtxgnl.mongodb.net/`);
+        await mongoose.connect(
+  `mongodb+srv://${username}:${password}@cluster0.sxtxgnl.mongodb.net/myDatabaseName?retryWrites=true&w=majority`
+);
+
         // await mongoose.connect('mongodb://127.0.0.1:27017/minor_project');
 
         console.log("Connected Succefully");
